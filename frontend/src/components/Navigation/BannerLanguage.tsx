799de773e515d30b1ba6542ab7e79d92c4fc09fe
benjaminmehrez <benjaminjofre01@gmail.com>
2025-05-31 18:15:05 -0300

    test

--- frontend/src/components/Navigation/BannerLanguage.tsx
+++ frontend/src/components/Navigation/BannerLanguage.tsx
@@ -1,29 +1,23 @@
 import useLanguageStore from "@/store/useLanguage";
 import { LanguageIcon } from "@heroicons/react/24/solid";
-import { Toaster, toast } from "sonner";
 
 function BannerLanguage() {
   const { language, setLanguage } = useLanguageStore();
   const handleClick = () => {
     if (language === "es") {
       setLanguage("en");
-      toast("English");
     } else {
       setLanguage("es");
-      toast("Español");
-    } 
+    }
   };
 
   return (
-    <>
-      <Toaster style={{ width: "115px", margin: "0 auto" }} position="top-center" />
-      <button
-        onClick={handleClick}
-        className="fixed right-8 bottom-10 border border-purple-600 bg-color rounded-full p-3 z-50 sm:hidden"
-      >
-        <LanguageIcon className="w-8 h-8 " />
-      </button>
-    </>
+    <button
+      onClick={handleClick}
+      className="fixed right-8 bottom-10 border border-purple-600 bg-color rounded-full p-3 z-50 sm:hidden"
+    >
+      <LanguageIcon className="w-8 h-8 " />
+    </button>
   );
 }
 
